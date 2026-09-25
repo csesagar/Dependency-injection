2d3be2463c3687bc354db2cd400925a04c12eebd
sagar paswan <csesagar55@gmail.com>
2026-09-25 23:46:24 +0530

    Add project files.

---
+++ MVCDHProject/Models/CustomerSQLDAL.cs
@@ -0,0 +1,31 @@
+﻿namespace MVCDHProject.Models
+{
+    public class CustomerSQLDAL : ICustomerDAL
+    {
+        public List<Customer> Customers_Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Customer_Delete(int Custid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Customer_Insert(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Customer_Select(int Custid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Customer_Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+ 
